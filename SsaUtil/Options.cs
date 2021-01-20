6c8b40337972873c4880c0c291a2b5ea1b0c3109
Maxstupo <maxstupo@hotmail.com>
2021-01-20 15:19:44 +1100

    Add basic cli structure

---
+++ SsaUtil/Options.cs
@@ -0,0 +1,33 @@
+﻿namespace Maxstupo.SsaUtil {
+
+    using System.Collections.Generic;
+    using CommandLine;
+
+    public abstract class BaseOptions {
+
+        [Option('i', "input", HelpText = "<TODO>")]
+        public IEnumerable<string> Inputs { get; set; }
+
+        [Option('r', "recursive", HelpText = "<TODO>")]
+        public bool Recursive { get; set; }
+
+    }
+
+
+    [Verb("edit", HelpText = "<TODO>")]
+    public sealed class EditOptions : BaseOptions {
+
+
+
+    }
+
+
+    [Verb("info", HelpText = "<TODO>")]
+    public sealed class InfoOptions : BaseOptions {
+
+
+
+    }
+
+
+}
--- SsaUtil/Program.cs
+++ SsaUtil/Program.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Maxstupo.SsaUtil {
 
-namespace Maxstupo.SsaUtil {
-    class Program {
-        static void Main(string[] args) {
+    using System;
+    using CommandLine;
+    using Maxstupo.SsaUtil.Utility;
+
+    public sealed class Program {
+
+        private IOutput Output { get; } = new ColorConsole();
+
+        private void Init(BaseOptions options) {
+            Output.WriteLine(Level.Info, "Hello World");
+            Output.WriteLine(Level.Warn, "Hello World");
+            Output.WriteLine(Level.Severe, "Hello World");
+            Output.WriteLine(Level.Fine, "Hello World");
         }
+
+        private int RunEdit(EditOptions options) {
+
+            return 0;
+        }
+
+        private int RunInfo(InfoOptions options) {
+
+            return 0;
+        }
+
+        private static int Main(string[] args) {
+
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+                args = "info".Split(' ');
+#endif
+
+            Program program = new Program();
+
+            return OnReturnCode(Parser.Default.ParseArguments<InfoOptions, EditOptions>(args)
+                .MapResult(
+                    (InfoOptions options) => { program.Init(options); return program.RunInfo(options); },
+                    (EditOptions options) => { program.Init(options); return program.RunEdit(options); },
+                    errors => -1
+                ));
+        }
+
+        private static int OnReturnCode(int returnCode) {
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+                Console.ReadKey();
+#endif
+            return returnCode;
+        }
+
     }
+
 }
