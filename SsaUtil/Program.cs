9de49c2be1a2fea6229523d21f77aba1ef2cf38f
Maxstupo <maxstupo@hotmail.com>
2021-01-20 14:45:15 +1100

    Initial commit

---
+++ SsaUtil/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maxstupo.SsaUtil {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
